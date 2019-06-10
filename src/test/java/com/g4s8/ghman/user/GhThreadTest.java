f4622cb1196d3020a82606eb41f7f4a6c75da8a7
olenagerasimova <agerasimova@zagruzka.com>
2019-06-10 23:34:57 +0300

    #8 set up qulice and test coverage

--- src/test/java/com/g4s8/ghman/user/GhThreadTest.java
+++ src/test/java/com/g4s8/ghman/user/GhThreadTest.java
@@ -16,10 +16,12 @@
  */
 package com.g4s8.ghman.user;
 
+import java.time.Instant;
 import javax.json.Json;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
+import javax.json.JsonObject;
+import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
+import org.llorllale.cactoos.matchers.Assertion;
 
 /**
  * Test case for {@link GhThread}.
@@ -32,11 +34,48 @@ public final class GhThreadTest {
     @Test
     void parseId() {
         final String tid = "4";
-        MatcherAssert.assertThat(
+        new Assertion<>(
+            "Failed to get thread id from json",
             new GhThread(
                 Json.createObjectBuilder().add("id", tid).build()
             ).tid(),
-            Matchers.equalTo(tid)
-        );
+            new IsEqual<>(tid)
+        ).affirm();
+    }
+
+    @Test
+    void returnsSubject() {
+        final JsonObject subj = Json.createObjectBuilder()
+            .add("title", "issue #3").add("link", "http://issues/3").build();
+        new Assertion<>(
+            "Failed to get subject from json",
+            new GhThread(
+                Json.createObjectBuilder().add("subject", subj).build()
+            ).subject(),
+            new IsEqual<>(subj)
+        ).affirm();
+    }
+
+    @Test
+    void returnsLastRead() {
+        final String date = "2019-12-03T10:15:30.00Z";
+        new Assertion<>(
+            "Failed to get last read from json",
+            new GhThread(
+                Json.createObjectBuilder().add("last_read_at", date).build()
+            ).lastRead(),
+            new IsEqual<>(Instant.parse(date))
+        ).affirm();
+    }
+
+    @Test
+    void returnsEpochWhenLastReadIsAbsent() {
+        new Assertion<>(
+            "Failed to get Instant.EPOCH as last read",
+            new GhThread(
+                Json.createObjectBuilder().build()
+            ).lastRead(),
+            new IsEqual<>(Instant.EPOCH)
+        ).affirm();
     }
 }
